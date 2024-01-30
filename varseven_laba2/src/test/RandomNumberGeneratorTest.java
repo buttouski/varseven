b9a7c84fa79566dce2a19e8eb5f2f7d1b5cf8cfc
DanilDanilDanil <danil_bikbaev@mail.ru>
2024-01-30 22:54:14 +0500

    Добавил юнит тест для класса

---
+++ varseven_laba2/src/test/RandomNumberGeneratorTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class RandomNumberGeneratorTest {
+
+  @Test
+    public void test() {
+        int[] randomNumbers = generateRandomNumbers();
+        for (int number : randomNumbers) {
+            assertTrue(number >= 100 && number <= 500);
+        }
+    }
+
+    private int[] generateRandomNumbers() {
+        int[] randomNumbers = new int[11];
+        Random random = new Random();
+        for (int i = 0; i < randomNumbers.length; i++) {
+            randomNumbers[i] = random.nextInt(401) + 100;
+        }
+        return randomNumbers;
+    }
+
+}
